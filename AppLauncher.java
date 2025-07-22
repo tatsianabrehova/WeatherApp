d8cf69d42894f1a04788485f16cddd88e4ad4d5c
tatsianabrehova <tatsianabrehova@gmail.com>
2025-07-22 23:38:09 +0300

    done

--- AppLauncher.java
+++ AppLauncher.java
@@ -7,7 +7,7 @@ public static void main(String[] args){
         public void run (){
             new WeatherApp().setVisible(true);
 //System.out.println(App.getLocationData("Tokyo"));
-            System.out.println(App.getCurrentTime());
+            System.out.println( App.getCurrentTime());
         }
     });
 }
